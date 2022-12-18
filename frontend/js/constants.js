55880be88ca94c5ee1d26f7598fa07662c29fbbe
UltraSilvafine <35620003+levirtualle@users.noreply.github.com>
2022-12-18 22:02:39 +0800

    edit minting dapp

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0xA96A377A55Fb768E2A1D96E5f0a266B71acc14D8"; // Replace with your own contract address
 const chain = 'ethereum'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "Welcome to Unknown Federation NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'Unknown Federation NFT Project is a NFT collection of cadets and officers that is inspired by famous sci-fi tv series like Star Trek and The Orville. The owner of this NFT collection will get lifetime access of a FREE mutiple choice, multiple ending animated 3D interactive novel that will be develop if we manage to sold out this collection. The characters that you own will be the star of that novel. Owners will also get free print products (Terms and conditions applies) and giveaways such as Airdrop NFTs that will be conducted in our Discord server. Join our Discord by clicking the icon above.';
+const welcome_h1 = "";
+const welcome_h2 = "Connect Your Wallet to Start Minting";
+const welcome_p = '';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
